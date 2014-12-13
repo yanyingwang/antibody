87820b0b06f885dfe28b462df8eb5c2332f9db24
YanyingWang <yanyingwangwang@gmail.com>
2014-12-14 02:07:55 +0800

    antibody cmd

---
+++ antibody.zsh
@@ -0,0 +1,64 @@
+#!/usr/bin/env zsh
+
+set -e
+
+#antibody list
+#antibody use github yanyingwang/waga
+#antibody use oh-my-zsh 
+
+
+list()
+{
+  echo $fg[green]"Plugins: \n\n"
+  for d in $(ls bundle/)
+  do
+    echo "---: ${d/#*bundle\/}"
+  done
+}
+
+
+github()
+{
+  test -d bundle/$plugin_dir || git clone https://github.com/$p2.git bundle/$plugin_dir
+  source bundle/$plugin_dir/*.plugin.zsh
+}
+oh-my-zsh()
+{
+  test -d bundle/oh-my-zsh || git clone https://github.com/robbyrussell/oh-my-zsh.git bundle/oh-my-zsh
+  source bundle/oh-my-zsh/plugins/$plugin/*.plugin.zsh
+}
+
+use()
+{
+  plugin_dir=${endpoint}-${plugin/\//-}
+
+  if [[ $endpoint = 'github' ]]
+  then
+    github
+  elif [[ $endpoint = 'oh-my-zsh' ]]
+  then
+    oh-my-zsh
+  fi
+}
+
+
+antibody()
+{
+  PWD=${${0:a:h}%bundle*}
+  action=$1
+  endpoint=$2
+  plugin=$3
+
+  case $action in
+    list)
+      list
+      ;;
+    use)
+      use
+      ;;
+  esac
+}
+
+
+
+
